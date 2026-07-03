2ff31fc285b5fd88a4052d8e1735c2473c230d3b
Sudhanshu Pandey <chunky@Sudhanshus-MacBook-Pro.local>
2026-07-03 23:10:35 +0900

    products map

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,30 @@
-import Header from "./components/Header";
 import ProductCard from "./components/ProductCard";
+import { products } from "./api/products";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div>
+    <div> 
       <Header/>
-      <ProductCard/>
+    <div
+      style={{
+        display: "flex",
+        gap: "10px",
+        justifyContent: "center",
+        marginTop: "50px",
+        flexWrap: "wrap"
+      }}
+    >
+      {products.map((item) => (
+        <ProductCard
+          key={item.id}
+          image={item.image}
+          name={item.name}
+          price={item.price}
+          link={item.link}
+        />
+      ))}
+    </div>
     </div>
   );
 }
