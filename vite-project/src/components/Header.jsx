281fe28d1e1adb55d8c5a9e99d562899d38aa854
Sudhanshu Pandey <chunky@Sudhanshus-MacBook-Pro.local>
2026-07-29 20:30:28 +0900

    title change

--- vite-project/src/components/Header.jsx
+++ vite-project/src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <header>
-      <h1>SUNDU SHOP</h1>
+      <h1>SUNDU PICK</h1>
       <p>내가 직접 써보고 추천하는 제품들</p>
       <p>📣 파트너스 활동의 일환으로 일정액의 수수료를 지급받습니다.</p>
 
